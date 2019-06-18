2261ddd96483bc4fb6cc68dff7c22e713a6f8bd4
Fa-BRAIK <faroukbraik17@gmail.com>
2019-06-18 16:20:33 +0100

    Migrating to one draw function

--- js/draw.js
+++ js/draw.js
@@ -1,22 +1,13 @@
 import { mapArray } from './map.js'
 
-export const drawMap = (ctx) => {
+export const drawMap = (ctx, callback) => {
     const tiles = []
     
     loadImages(ctx, mapArray, tiles => {
         draw(ctx, tiles, mapArray)
+
+        return callback(tiles)
     })
-}
-
-export const drawPlayer = (ctx, mainCharacter) => {
-    const tileWidth = 128, tileHeight = 256 // the default size of the loaded tiles is 256 * 512 
-
-    const mapX = 450, mapY = -128 //mapX and mapY are offsets to positioning the map 
-
-    ctx.drawImage(mainCharacter.state, 
-        (mainCharacter.positionX - mainCharacter.positionY) * tileWidth/2 + mapX, 
-        (mainCharacter.positionX + mainCharacter.positionY) * tileWidth / 4 + mapY, 
-        tileWidth, tileHeight);
 }
 
 const loadImages = (ctx, mapArray, callback) => {
@@ -62,21 +53,31 @@ const draw = (ctx, tiles, mapArray) => {
                 case 2: { tile_id = Math.floor(Math.random() * 4); level2Tiles.push({ x: i, y: j}); break } //2 is a crate it is stored as a level 2 building
                 case 3: { tile_id = Math.floor(Math.random() * 4); level3Tiles.push({ x: i, y: j}); break } //3 is a gate it is stored as a level 3 building
                 default: {
-                    tile_id = (mapArray[i][j] === 0) ? Math.floor(Math.random() * 4)
+                    tile_id = (mapArray[i][j] === 0) 
+                        ? Math.floor(Math.random() * 4)
                         : Math.floor(Math.random() * 4) + 4
                 } 
             }
 
-            ctx.drawImage(tiles[tile_id], (i - j) * tileWidth/2 + mapX, (i + j) * tileWidth / 4 + mapY, tileWidth, tileHeight);
+            ctx.drawImage(tiles[tile_id], 
+                (i - j) * tileWidth/2 + mapX, 
+                (i + j) * tileWidth / 4 + mapY, 
+                tileWidth, tileHeight);
     }
 
     // time to draw the level 2 priority
     tile_id = 8
     for (let i = 0; i < level2Tiles.length; i++) 
-        ctx.drawImage(tiles[tile_id], (level2Tiles[i].x - level2Tiles[i].y) * tileWidth/2 + mapX, (level2Tiles[i].x + level2Tiles[i].y) * tileWidth / 4 + mapY, tileWidth, tileHeight);
+        ctx.drawImage(tiles[tile_id], 
+            (level2Tiles[i].x - level2Tiles[i].y) * tileWidth / 2 + mapX, 
+            (level2Tiles[i].x + level2Tiles[i].y) * tileWidth / 4 + mapY, 
+            tileWidth, tileHeight);
 
     // time to draw the level 3 priority
     tile_id = 9
     for (let i = 0; i < level3Tiles.length; i++)
-        ctx.drawImage(tiles[tile_id], (level3Tiles[i].x - level3Tiles[i].y) * tileWidth/2 + mapX, (level3Tiles[i].x + level3Tiles[i].y) * tileWidth / 4 + mapY, tileWidth, tileHeight);
+        ctx.drawImage(tiles[tile_id], 
+            (level3Tiles[i].x - level3Tiles[i].y) * tileWidth / 2 + mapX, 
+            (level3Tiles[i].x + level3Tiles[i].y) * tileWidth / 4 + mapY, 
+            tileWidth, tileHeight);
 }
--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
-import { drawMap, drawPlayer } from './draw.js'
+import { drawMap } from './draw.js'
 import { Player } from './player.js'
 
 const canvas = document.querySelector('#index_canvas')
 let ctx = canvas.getContext('2d')
 
 Player.loadPlayerStates(playerStates => {
-    let mainCharacter = new Player(1, 1, playerStates.idle[3])
+    let mainCharacter = new Player(2, 2, playerStates.idle[3])
     
-    drawMap(ctx) // draw the map
-    // drawPlayer(ctx, mainCharacter) // after drawing the map we'll draw the player
+    drawMap(ctx, tiles => {
+        console.log(tiles)
+        //drawPlayer(ctx, tiles, mainCharacter) // after drawing the map we'll draw the player
+    }) // draw the map 
+    
+
 })
